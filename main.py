5c3c838e5b89d72f3be6f2a44287814d2f61ee6c
GreasyRooster <greasyRooster1@gmail.com>
2026-01-20 19:15:02 -0800

    test

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import os
 
+from DeepSeekCoderV2 import DeepSeekCoderV2
 
+model = DeepSeekCoderV2()
+model.init_model()
+
+model.send_model_request("what is the purpose of ralph wiggums in the context of AI?")
