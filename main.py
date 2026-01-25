fdd12e7f987bd95c2aa667baa9750968b4c99050
GreasyRooster <greasyRooster1@gmail.com>
2026-01-24 19:11:09 -0800

    generate to file

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ import os
 
 from DeepSeekCoderV2 import DeepSeekCoderV2
 from dotenv import load_dotenv
+import git
 
+temp_path="./temp"
+main_file_name = "main.py"
 
 load_dotenv()
 
 programmer_model = DeepSeekCoderV2()
 programmer_model.init_model()
 
-programmer_model.sys_prompt = "You are an intelligent programmer, you only ever output code, never any commentary on your code, unless in the form of code comments. You write in JavaScript, you use the P5.js library. The boilerplate html for p5js is already written. your code goes directly into a script tag. remember to always define your variables and use correct syntax. Leave comments annotating your code frequently"
+programmer_model.sys_prompt = "You are an intelligent programmer, you only ever output code, never any commentary on your code, unless in the form of code comments. You write in JavaScript, you use the P5.js library. The boilerplate html for p5js is already written. your code goes directly into a script tag. remember to always define your variables and use correct syntax. Leave comments annotating your code frequently. do not output your code with markdown styling"
 
 stream = programmer_model.stream_model_request("make a square bounce on the screen")
 
-for chunk in stream:
-    print(chunk['message']['content'], end='', flush=True)
+def generate_to_file(message,model):
+    with open(temp_path+"/"+main_file_name, 'w') as f:
+        for chunk in stream:
+            val = chunk['message']['content']
+            f.write(val)
+            print(chunk['message']['content'], end='', flush=True)
