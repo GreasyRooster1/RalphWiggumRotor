92b8a0d9a1c3a3a7a97875407a5210e66b15ec14
GreasyRooster <greasyRooster1@gmail.com>
2026-01-24 18:44:48 -0800

    sys prompt

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-model = DeepSeekCoderV2()
-model.init_model()
+programmer_model = DeepSeekCoderV2()
+programmer_model.init_model()
 
-stream = model.stream_model_request("what is the purpose of ralph wiggums in the context of AI?")
+programmer_model.sys_prompt = "You are an intelligent programmer, you only ever output code, never any commentary on your code, unless in the form of code comments. You write in JavaScript, you use the P5.js library. The boilerplate html for p5js is already written. your code goes directly into a script tag. remember to always define your variables and use correct syntax. Leave comments annotating your code frequently"
+
+stream = programmer_model.stream_model_request("make a square bounce on the screen")
+
 for chunk in stream:
     print(chunk['message']['content'], end='', flush=True)
