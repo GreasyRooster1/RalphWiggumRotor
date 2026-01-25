eeacceb42e33a4079c076dce686824445e193f7f
GreasyRooster <greasyRooster1@gmail.com>
2026-01-24 19:12:07 -0800

    func

--- main.py
+++ main.py
@@ -9,15 +9,19 @@ main_file_name = "main.py"
 
 load_dotenv()
 
-programmer_model = DeepSeekCoderV2()
-programmer_model.init_model()
 
-programmer_model.sys_prompt = "You are an intelligent programmer, you only ever output code, never any commentary on your code, unless in the form of code comments. You write in JavaScript, you use the P5.js library. The boilerplate html for p5js is already written. your code goes directly into a script tag. remember to always define your variables and use correct syntax. Leave comments annotating your code frequently. do not output your code with markdown styling"
 
-stream = programmer_model.stream_model_request("make a square bounce on the screen")
+def main():
+    programmer_model = DeepSeekCoderV2()
+    programmer_model.init_model()
 
-def generate_to_file(message,model):
-    with open(temp_path+"/"+main_file_name, 'w') as f:
+    programmer_model.sys_prompt = "You are an intelligent programmer, you only ever output code, never any commentary on your code, unless in the form of code comments. You write in JavaScript, you use the P5.js library. The boilerplate html for p5js is already written. your code goes directly into a script tag. remember to always define your variables and use correct syntax. Leave comments annotating your code frequently. do not output your code with markdown styling"
+
+
+
+def generate_to_file(message,model,path):
+    stream = model.stream_model_request("make a square bounce on the screen")
+    with open(path, 'w') as f:
         for chunk in stream:
             val = chunk['message']['content']
             f.write(val)
